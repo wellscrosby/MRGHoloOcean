0c87710337c99e2b069e74786f69a7a8c7a88e2e
Easton Potokar <contagon6@gmail.com>
2020-09-18 04:06:52 -0600

    Added rotation
    Former-commit-id: c590210615e76d074311499c1211c547ee3de43e

--- engine/Source/Holodeck/Sensors/Private/DVLSensor.cpp
+++ engine/Source/Holodeck/Sensors/Private/DVLSensor.cpp
@@ -16,7 +16,11 @@ void UDVLSensor::InitializeSensor() {
 void UDVLSensor::TickSensorComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) {
 	//check if your parent pointer is valid, and if the sensor is on. Then get the velocity and buffer, then send the data to it. 
 	if (Parent != nullptr && bOn) {
-		FVector Velocity = Parent->GetVelocity();
+		//UE4 gives us world velocity, rotate it to get local velocity
+		FRotator R = Parent->GetActorRotation();
+		FVector Velocity = R.UnrotateVector(Parent->GetVelocity());
+
+		//Convert and send to buffer
 		Velocity = ConvertLinearVector(Velocity, UEToClient);
 		float* FloatBuffer = static_cast<float*>(Buffer);
 		FloatBuffer[0] = Velocity.X;
