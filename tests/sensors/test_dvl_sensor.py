61545e3a87e600010b236baf163a56e186cf7036
Easton Potokar <contagon6@gmail.com>
2021-01-02 19:41:42 -0700

    Fixed DVL test

--- tests/sensors/test_dvl_sensor.py
+++ tests/sensors/test_dvl_sensor.py
@@ -30,18 +30,19 @@ def test_dvl_sensor_straight():
 
     with holodeck.environments.HolodeckEnvironment(scenario=turtle_config,
                                                    binary_path=binary_path,
-                                                   show_viewport=True,
+                                                   show_viewport=False,
                                                    uuid=str(uuid.uuid4())) as env:
         #let it land and then start moving forward
         for _ in range(100):
-            last_x_velocity, _ = env.tick()["DVLSensor"]
+            last_x_velocity, _, _ = env.tick()["DVLSensor"]
         env.step([150,0])
 
         #Move forward, making sure y is relatively small, and x is increasing
         for i in range(50):
-            new_x_velocity, y_velocity = env.step([150,0])[0]["DVLSensor"]
+            new_x_velocity, y_velocity, z_velocity = env.step([150,0])[0]["DVLSensor"]
             assert new_x_velocity >= last_x_velocity, f"The velocity didn't increase at step {i}!"
             assert y_velocity <= .5
+            assert z_velocity <= .5
             last_x_velocity = new_x_velocity
 
         #Let it stop
@@ -50,9 +51,10 @@ def test_dvl_sensor_straight():
 
         #Move backward, making sure y is relatively small, and x is decreasing
         for i in range(50):
-            new_x_velocity, y_velocity = env.step([-150,0])[0]["DVLSensor"]
+            new_x_velocity, y_velocity, z_velocity = env.step([-150,0])[0]["DVLSensor"]
             assert new_x_velocity <= last_x_velocity, f"The velocity didn't decrease at step {i}!"
             assert y_velocity <= .5
+            assert z_velocity <= .5
             last_x_velocity = new_x_velocity
 
         #Let it stop
@@ -60,9 +62,10 @@ def test_dvl_sensor_straight():
             env.step([0, 0])
 
         #make sure everythign is close to 0
-        x_velocity, y_velocity = env.tick()["DVLSensor"]
+        x_velocity, y_velocity, z_velocity = env.tick()["DVLSensor"]
         assert x_velocity <= 1e-2, "The x velocity wasn't close enough to zero!"
         assert y_velocity <= 1e-2, "The y velocity wasn't close enough to zero!"
+        assert z_velocity <= 1e-2, "The z velocity wasn't close enough to zero!"
 
 def test_dvl_sensor_rotated():
     """Make sure when we move forward x is positive, y is close to 0. Make sure when not moving both are close to 0
@@ -77,14 +80,15 @@ def test_dvl_sensor_rotated():
                                                    uuid=str(uuid.uuid4())) as env:
         #let it land and then start moving forward
         for _ in range(100):
-            last_x_velocity, _ = env.tick()["DVLSensor"]
+            last_x_velocity, _, _ = env.tick()["DVLSensor"]
         env.step([150,0])
 
         #Move forward, making sure y is relatively small, and x is increasing
         for i in range(60):
-            new_x_velocity, y_velocity = env.step([150,0])[0]["DVLSensor"]
+            new_x_velocity, y_velocity, z_velocity = env.step([150,0])[0]["DVLSensor"]
             assert new_x_velocity >= last_x_velocity, f"The velocity didn't increase at step {i}!"
             assert y_velocity <= .5
+            assert z_velocity <= .5
             last_x_velocity = new_x_velocity
 
         #Let it stop
@@ -93,9 +97,10 @@ def test_dvl_sensor_rotated():
 
         #Move backward, making sure y is relatively small, and x is decreasing
         for i in range(20):
-            new_x_velocity, y_velocity = env.step([-150,0])[0]["DVLSensor"]
+            new_x_velocity, y_velocity, z_velocity = env.step([-150,0])[0]["DVLSensor"]
             assert new_x_velocity <= last_x_velocity, f"The velocity didn't decrease at step {i}!"
             assert y_velocity <= .5
+            assert z_velocity <= .5
             last_x_velocity = new_x_velocity
 
         #Let it stop
@@ -103,6 +108,7 @@ def test_dvl_sensor_rotated():
             env.step([0, 0])
 
         #make sure everythign is close to 0
-        x_velocity, y_velocity = env.tick()["DVLSensor"]
+        x_velocity, y_velocity, z_velocity = env.tick()["DVLSensor"]
         assert x_velocity <= 1e-2, "The x velocity wasn't close enough to zero!"
         assert y_velocity <= 1e-2, "The y velocity wasn't close enough to zero!"
+        assert z_velocity <= 1e-2, "The z velocity wasn't close enough to zero!"
