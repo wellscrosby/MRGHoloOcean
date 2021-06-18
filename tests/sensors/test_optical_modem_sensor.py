1d3a325f410dc9094229fef401e8fe73d33c5160
SpencerAshford <spencer@theashfords.org>
2021-06-18 10:41:43 -0600

    Added test for line of sight checking when there is an obstruction.

--- tests/sensors/test_optical_modem_sensor.py
+++ tests/sensors/test_optical_modem_sensor.py
@@ -184,3 +184,66 @@ def test_not_oriented():
         env.agents.get("uav0")._client.command_center.enqueue_command(SendOpticalMessageCommand("uav0","OpticalModemSensor", "uav1", "OpticalModemSensor"))
 
         assert env.agents.get("uav1").sensors.get("OpticalModemSensor").sensor_data == None, "Receiving modem received data when it should not have done so."
+
+uav_config_v4 = {
+    "name": "test",
+    "world": "TestWorld",
+    "main_agent": "uav0",
+    "agents": [
+        {
+            "agent_name": "uav0",
+            "agent_type": "UavAgent",
+            "sensors": [
+                {
+                    "sensor_type": "LocationSensor",
+                },
+                {
+                    "sensor_type": "VelocitySensor"
+                },
+                {
+                    "sensor_type": "RGBCamera"
+                },
+                {
+                    "sensor_type": "OpticalModemSensor",
+                    "configuration": {
+                        "MaxDistance": 10
+                    }
+                }
+            ],
+            "control_scheme": 1,
+            "location": [-3, 4.5, .25],
+            "rotation": [0, 0, -90]
+        },
+        {
+            "agent_name": "uav1",
+            "agent_type": "UavAgent",
+            "sensors": [
+                {
+                    "sensor_type": "OpticalModemSensor",
+                    "configuration": {
+                        "MaxDistance": 10
+                    }
+                }
+            ],
+            "control_scheme": 1,
+            "location": [-3, -4.5, .25],
+            "rotation": [0, 0, 90]
+        }
+    ]
+}
+
+def test_obstructed_view():
+"""Tests to ensure that modem is unable to transmit when there is an obstruction between modems."""
+    binary_path = holodeck.packagemanager.get_binary_path_for_package("DefaultWorlds")
+
+    with holodeck.environments.HolodeckEnvironment(scenario=uav_config_v4,
+                                                   binary_path=binary_path,
+                                                   show_viewport=False,
+                                                   uuid=str(uuid.uuid4())) as env:
+        command = [0, 0, 10, 50]
+
+        for _ in range(300):
+            state, reward, terminal, _ = env.step(command)
+            env.agents.get("uav0")._client.command_center.enqueue_command(SendOpticalMessageCommand("uav0","OpticalModemSensor", "uav1", "OpticalModemSensor"))
+
+        assert env.agents.get("uav1").sensors.get("OpticalModemSensor").sensor_data == None, "Receiving modem received data when it should not have done so."
