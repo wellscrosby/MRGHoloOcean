a6e4c519a034c152f93a0986903782de423886b8
SpencerAshford <spencer@theashfords.org>
2021-06-18 15:29:27 -0600

    debugging

--- tests/sensors/test_optical_modem_sensor.py
+++ tests/sensors/test_optical_modem_sensor.py
@@ -246,3 +246,58 @@ def test_obstructed_view():
             env.agents.get("uav0")._client.command_center.enqueue_command(SendOpticalMessageCommand("uav0","OpticalModemSensor", "uav1", "OpticalModemSensor"))
 
         assert env.agents.get("uav1").sensors.get("OpticalModemSensor").sensor_data == None, "Receiving modem received data when it should not have done so."
+
+base_cfg = {
+    "name": "test_rgb_camera",
+    "world": "TestWorld",
+    "main_agent": "sphere0",
+    "agents": [
+        {
+            "agent_name": "sphere0",
+            "agent_type": "SphereAgent",
+            "sensors": [
+                {
+                    "sensor_type": "RGBCamera",
+                    "socket": "CameraSocket",
+                    # note the different camera name. Regression test for #197
+                    "sensor_name": "TestCamera"
+                }
+            ],
+            "control_scheme": 0,
+            "location": [.95, -1.75, .5] # if you change this, you must change rotation_env too.
+        }
+    ]
+}
+
+
+def test_rgb_camera(resolution, request):
+    """Makes sure that the RGB camera is positioned and capturing correctly.
+
+    Capture pixel data, and load from disk the baseline of what it should look like.
+    Then, use mse() to see how different the images are.
+
+    """
+    global base_cfg
+
+    cfg = copy.deepcopy(base_cfg)
+
+    cfg["agents"][0]["sensors"][0]["configuration"] = {
+        "CaptureWidth": resolution,
+        "CaptureHeight": resolution
+    }
+
+    binary_path = holodeck.packagemanager.get_binary_path_for_package("DefaultWorlds")
+
+    with holodeck.environments.HolodeckEnvironment(scenario=cfg,
+                                                   binary_path=binary_path,
+                                                   show_viewport=False,
+                                                   uuid=str(uuid.uuid4())) as env:
+
+        for _ in range(5):
+            env.tick()
+
+        pixels = env.tick()['TestCamera'][:, :, 0:3]
+        baseline = cv2.imread(os.path.join(request.fspath.dirname, "expected", "{}.png".format(resolution)))
+        err = mean_square_err(pixels, baseline)
+
+        assert err < 2000
