537bb81eacdeb702587ea5a84bf939b7ac63789c
Easton Potokar <contagon6@gmail.com>
2021-07-06 12:33:25 -0600

    Small packaging changes
    Former-commit-id: 4a14bef48f2a18febc60cbbe34294f9a59f8d8f6

--- engine/Build/ContinuousIntegration/package_ue4_project.sh
+++ engine/Build/ContinuousIntegration/package_ue4_project.sh
@@ -5,6 +5,8 @@
 ue4 setroot /home/ue4/UnrealEngine
 
 packagename="Ocean"
+commit="$1"
+branch="$2"
 
 echo "⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠"
 echo "⚠ Packaging $packagename..."
@@ -21,7 +23,7 @@ ue4 package Development
 # Make sure it worked
 code=$?
 if [ code -ne 0 ]; then
-    >&2 echo "(╯°□°)╯︵ ┻━┻ Packaging $packagename failed with code $code!"
+    >&2 echo "(╯°□°)╯︵ ┻━┻ Packaging $commit failed with code $code!"
     exit $code
 fi
 
@@ -36,17 +38,19 @@ cd dist
 echo "👉 Copying config files into output directory..."
 cp ../Content/Worlds/Config/*.json .
 
-echo "👉 Compressing contents into $packagename.zip..."
-zip -r "$packagename.zip" *
+echo "👉 Compressing contents into $commit.zip..."
+zip -r "$commit.zip" *
 
-echo "👉 Moving $packagename.zip out of dist/ folder..."
-mv "$packagename.zip" ..
-
-echo "👉 Deleting config files for $packagename..."
+echo "👉 Deleting config files for $commit..."
 rm *.json
-
 cd ..
 
-echo "👉 Done packaging package $packagename"
+echo "👉 Moving $commit.zip into $packagename folder..."
+mkdir $packagename
+mv "dist/$commit.zip" $packagename
+cp $packagename/$commit.zip $packagename/latest.zip
+
+
+echo "👉 Done packaging package $commit"
 
 echo "👉 Sucessfully packaged all the packages 🎉🎉"
