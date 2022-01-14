Removed old dependencies, fix rotation tests

tests/scenarios/test_rotations.py
@@ -3,7 +3,6 @@ import uuid
 import pytest
 import numpy as np
 from scipy.spatial.transform import Rotation
-from tests.utils.equality import almost_equal
 
 @pytest.fixture(scope="module")
 def env():
@@ -49,7 +48,7 @@ def test_actor_rotation(env, num):
 
     state = env.tick()
 
-    assert np.allclose(R, state["OrientationSensor"])
+    assert np.allclose(R, state["OrientationSensor"], rtol=1e-3)
 
 
 @pytest.mark.parametrize('num', range(3))
@@ -67,7 +66,7 @@ def test_sensor_rotation(env, num):
 
     state = env.tick()
 
-    assert almost_equal(R, state["OrientationSensor"])
+    assert np.allclose(R, state["OrientationSensor"], rtol=1e-3)
 
 
 @pytest.mark.parametrize('num', range(3))
@@ -87,4 +86,4 @@ def test_teleport_rotation(env, num):
     env.agents['sphere'].teleport([0,0,0], angles.tolist())
     state = env.tick()
 
-    assert almost_equal(R, state["OrientationSensor"])
+    assert np.allclose(R, state["OrientationSensor"], rtol=1e-3)