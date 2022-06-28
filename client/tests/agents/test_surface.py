5ab1b44e309d715ff1147a8d3afaa895ebd5813e
Easton Potokar <contagon6@gmail.com>
2022-06-28 16:21:07 -0600

    Added surface vessel tests

--- client/tests/agents/test_surface.py
+++ client/tests/agents/test_surface.py
@@ -14,14 +14,14 @@ def env():
         "agents": [
             {
                 "agent_name": "auv0",
-                "agent_type": "HoveringAUV",
+                "agent_type": "SurfaceVessel",
                 "sensors": [
                     {
                         "sensor_type": "DynamicsSensor",
                     }
                 ],
                 "control_scheme": 0,
-                "location": [0,0,-10]
+                "location": [0,0,3]
             }
         ]
     }
@@ -34,21 +34,21 @@ def env():
         yield env
 
 
-def test_pid_controller(env):
-    """Test to make sure it goes to the orientation and position we command
-    """
-    des = [2, 2, -12, 0, 20, 45] 
-    env._scenario["agents"][0]["control_scheme"] = 1
+# def test_pid_controller(env):
+#     """Test to make sure it goes to the orientation and position we command
+#     """
+#     des = [2, 2, -12, 0, 20, 45] 
+#     env._scenario["agents"][0]["control_scheme"] = 1
 
-    env.reset()
+#     env.reset()
 
-    state = env.step(des, 200)
+#     state = env.step(des, 200)
 
-    pos = state["DynamicsSensor"][6:9]
-    rpy = state["DynamicsSensor"][15:18]
+#     pos = state["DynamicsSensor"][6:9]
+#     rpy = state["DynamicsSensor"][15:18]
 
-    assert np.allclose(des[:3], pos, 1e-1), "Controller didn't make it to the right position"
-    assert np.allclose(des[3:], rpy, 5), "Controller didn't make it to the right orientation"
+#     assert np.allclose(des[:3], pos, 1e-1), "Controller didn't make it to the right position"
+#     assert np.allclose(des[3:], rpy, 5), "Controller didn't make it to the right orientation"
 
 
 def test_manual_dynamics(env):
@@ -66,3 +66,28 @@ def test_manual_dynamics(env):
 
     assert np.allclose(des[:3], accel), "Manual dynamics didn't hit the correct linear acceleration"
     assert np.allclose(des[3:], ang_accel), "Manual dynamics didn't hit the correct angular acceleration"
+
+
+def test_buoyancy(env):
+    """Make sure it floats on the surface
+    """
+    # Going up
+    env._scenario["agents"][0]["control_scheme"] = 0
+    env._scenario["agents"][0]["location"] = [10,10,-5]
+
+    env.reset()
+    state = env.tick(100)
+    p = state["DynamicsSensor"][6:9]
+
+    assert p[2] > -1, "Surface Vessel didn't float"
+    assert p[2] < 1, "Surface Vessel didn't stop going up"
+
+    # Going down
+    env._scenario["agents"][0]["location"] = [10,10,5]
+
+    env.reset()
+    state = env.tick(100)
+    p = state["DynamicsSensor"][6:9]
+
+    assert p[2] > -1, "Surface Vessel didn't float"
+    assert p[2] < 1, "Surface Vessel didn't drop"
