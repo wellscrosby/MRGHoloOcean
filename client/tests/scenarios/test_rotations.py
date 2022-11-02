Fixed some rotation errors in tests

client/tests/scenarios/test_rotations.py
@@ -3,6 +3,11 @@ import uuid
 import pytest
 import numpy as np
 from scipy.spatial.transform import Rotation
+from scipy.linalg import logm
+
+def rot_error(A, B):
+    e = logm(A@B.T)
+    return np.array([e[0,1], e[0,2], e[1,2]])
 
 @pytest.fixture(scope="module")
 def env():
@@ -48,7 +53,7 @@ def test_actor_rotation(env, num):
 
     state = env.tick()
 
-    assert np.allclose(R, state["OrientationSensor"], rtol=1e-2)
+    assert np.allclose(np.zeros(3), rot_error(R, state["OrientationSensor"]))
 
 
 @pytest.mark.parametrize('num', range(3))
@@ -66,7 +71,7 @@ def test_sensor_rotation(env, num):
 
     state = env.tick()
 
-    assert np.allclose(R, state["OrientationSensor"], rtol=1e-2)
+    assert np.allclose(np.zeros(3), rot_error(R, state["OrientationSensor"]))
 
 
 @pytest.mark.parametrize('num', range(3))
@@ -86,4 +91,4 @@ def test_teleport_rotation(env, num):
     env.agents['sphere'].teleport([0,0,0], angles.tolist())
     state = env.tick()
 
-    assert np.allclose(R, state["OrientationSensor"], rtol=1e-2)
+    assert np.allclose(np.zeros(3), rot_error(R, state["OrientationSensor"]))