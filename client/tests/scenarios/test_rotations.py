Fixed tolerance on some rotation tests

client/tests/scenarios/test_rotations.py
@@ -53,7 +53,7 @@ def test_actor_rotation(env, num):
 
     state = env.tick()
 
-    assert np.allclose(np.zeros(3), rot_error(R, state["OrientationSensor"]))
+    assert np.allclose(np.zeros(3), rot_error(R, state["OrientationSensor"]), atol=1e-5)
 
 
 @pytest.mark.parametrize('num', range(3))
@@ -71,7 +71,7 @@ def test_sensor_rotation(env, num):
 
     state = env.tick()
 
-    assert np.allclose(np.zeros(3), rot_error(R, state["OrientationSensor"]))
+    assert np.allclose(np.zeros(3), rot_error(R, state["OrientationSensor"]), atol=1e-5)
 
 
 @pytest.mark.parametrize('num', range(3))
@@ -91,4 +91,4 @@ def test_teleport_rotation(env, num):
     env.agents['sphere'].teleport([0,0,0], angles.tolist())
     state = env.tick()
 
-    assert np.allclose(np.zeros(3), rot_error(R, state["OrientationSensor"]))
+    assert np.allclose(np.zeros(3), rot_error(R, state["OrientationSensor"]), atol=1e-5)