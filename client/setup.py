cff10008d43b44f0585fe18057170db428de0d8d
Kalin Norman <kalinnorman@gmail.com>
2023-06-07 23:47:07 +0000

    update python version requirements in setup.py

--- client/setup.py
+++ client/setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="holoocean",
-    version="0.5.0",
+    version="0.5.1",
     description="Autonomous Underwater Vehicle Simulator",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -15,7 +15,7 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     license='MIT License',
-    python_requires=">=3.6",
+    python_requires=">=3.7, <3.11",
     install_requires=[
         'posix_ipc >= 1.0.0; platform_system == "Linux"',
         'pywin32 <= 228; platform_system == "Windows"',
